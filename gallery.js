6f2954e8b48a3c9699a4b90be92fe7d502531bb6
jdhert <48412782+jdhert@users.noreply.github.com>
2019-04-15 03:35:30 +0900

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,32 @@
+
+
+var images = [];
+
+images.push("qwe.jpg");
+images.push("qwe1.jpg");
+images.push("qwe2.jpg");
+
+currentImage = 0;
+
+
+document.querySelector("#prevButton").addEventListener("click" ,
+    function(){
+        currentImage --;
+        if(currentImage<0)
+        currentimage = images.length-1;
+    showImage();
+    
+});
+document.querySelector("#nextButton").addEventListener("click" ,
+    function(){
+        currentImage ++;
+        if(currentImage>=images.length)
+            currentimage = 0;
+        showImage();
+});
+
+function showImage()
+{
+    pc = document.getElementById('picture');
+    pc.src = images[currentImage];
+}

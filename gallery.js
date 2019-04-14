f6a5ac46d9b4a537d37edc87d1f456b26944537e
jdhert <48412782+jdhert@users.noreply.github.com>
2019-04-15 03:46:51 +0900

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,32 +1,32 @@
 
-
 var images = [];
-
 images.push("qwe.jpg");
 images.push("qwe1.jpg");
 images.push("qwe2.JPG");
 
+
 currentImage = 0;
-
-
-document.querySelector("#prevButton").addEventListener("click" ,
+document.querySelector("#prevButton").addEventListener("click",
     function(){
+        //alert("ok");
         currentImage --;
         if(currentImage<0)
-        currentimage = images.length-1;
-    showImage();
-    
-});
-document.querySelector("#nextButton").addEventListener("click" ,
-    function(){
-        currentImage ++;
-        if(currentImage>=images.length)
-            currentimage = 0;
+            currentImage  = images.length -1 ;
         showImage();
 });
 
-function showImage()
-{
+document.querySelector("#nextButton").addEventListener("click",
+    function(){
+        //alert("ok");
+        currentImage ++;
+        if(currentImage>=images.length)
+            currentImage  = 0;
+
+        showImage();
+
+});
+
+function showImage(){
     pc = document.getElementById('picture');
     pc.src = images[currentImage];
 }
